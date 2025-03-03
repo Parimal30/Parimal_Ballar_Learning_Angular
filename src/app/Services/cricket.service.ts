344a377917ea74d59f18c7e4056cb3d193966274
“Parimal <W0851103@myscc.ca>
2025-03-03 18:01:54 -0500

    completed step 6 and 7

--- src/app/Services/cricket.service.ts
+++ src/app/Services/cricket.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from '@angular/core';
+import {Cricket} from '../Shared/Models/Cricket';
+import {Observable, of} from 'rxjs';
+import {crickets} from '../Shared/mockCricket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CricketService {
   constructor() { }
+  getCrickets(): Observable<Cricket[]> {
+    return of(crickets);
+  }
 }
--- src/app/cricket-list/cricket-list.component.ts
+++ src/app/cricket-list/cricket-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Cricket} from '../Shared/Models/Cricket';
 import {CricketListItemComponent} from '../cricket-list-item/cricket-list-item.component';
 import {NgClass, NgForOf} from '@angular/common';
+import {CricketService} from '../Services/cricket.service';
 
 @Component({
   selector: 'app-cricket-list',
@@ -11,13 +12,9 @@ import {NgClass, NgForOf} from '@angular/common';
   styleUrl: './cricket-list.component.css'
 })
 export class CricketListComponent {
-  cricket1: Cricket = {playerName: "Virat Kohli", playerJerseyNumber:18, playerPosition: "Batsman", playerAge: 35, isplayerawards: true, bgClass: "pink-bg"};
-  cricket2: Cricket = {playerName: "Rohit Sharma", playerPosition: "Batsman", playerJerseyNumber: 45,playerAge: 36, isplayerawards: true, bgClass: "teal-bg"};
-  cricket3: Cricket = {playerName: "Jasprit Bumrah", playerJerseyNumber: 93, playerPosition: "Bowler", playerAge: 30, isplayerawards: true, bgClass: "pink-bg"};
-  cricket4: Cricket = {playerName: "Ravindra Jadeja", playerJerseyNumber: 8, playerPosition: "All-Rounder", playerAge: 35, isplayerawards: true, bgClass: "teal-bg"};
-  cricket5: Cricket = {playerName: "KL Rahul", playerJerseyNumber: 1, playerPosition: "Wicketkeeper-Batsman", playerAge: 32, isplayerawards: true, bgClass: "pink-bg"};
-  cricket6: Cricket = {playerName: "Shubman Gill", playerJerseyNumber: 77, playerPosition: "Batsman", playerAge: 24, isplayerawards: true, bgClass: "teal-bg"};
 
-  cricketlist: Cricket[] = [this.cricket1, this.cricket2, this.cricket3, this.cricket4, this.cricket5, this.cricket6];
+  crickets: Cricket[] = [];
 
+  constructor(private cricketService: CricketService) {
+  }
 }
