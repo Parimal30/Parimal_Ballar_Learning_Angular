8437a4ce844fb3ad4bf134f9e1bfffa001f2e81d
“Parimal <W0851103@myscc.ca>
2025-03-03 18:16:13 -0500

    completed step 8 and 9

--- src/app/Services/cricket.service.ts
+++ src/app/Services/cricket.service.ts
@@ -7,8 +7,33 @@ import {crickets} from '../Shared/mockCricket';
   providedIn: 'root'
 })
 export class CricketService {
+
+  private cricketList: Cricket[] = crickets;
   constructor() { }
   getCrickets(): Observable<Cricket[]> {
     return of(crickets);
+  }
+
+  addCricket(newCricket: Cricket): Observable<Cricket> {
+    this.cricketList.push(newCricket);
+    return of(this.cricketList)
+  }
+
+  updateCricket(updateCricket: Cricket): Observable<Cricket> {
+    const index = this.cricketList.findIndex(foot => foot.playerName === updateCricket.playerName);
+    if (index !== -1) {
+      this.cricketList[index] = updateCricket;
+    }
+    return of(this.cricketList);
+  }
+
+  deleteCricket(deleteplayerName: string): Observable<Cricket[]> {
+    this.cricketList = this.cricketList.filter(foot => foot.playerName !== deleteplayerName);
+    return of(this.cricketList);
+  }
+
+  getfootball(readplayerName: string): Observable<Cricket | undefined> {
+    const cricket = this.cricketList.find(foot => foot.playerName === readplayerName);
+    return of(cricket);
   }
 }
