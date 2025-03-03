bed01653b62bf074d0b617a4939959a9c20deccf
“Parimal <W0851103@myscc.ca>
2025-03-03 17:57:22 -0500

    completed step 4 and 5

---
+++ src/app/Shared/mockCricket.ts
@@ -0,0 +1,11 @@
+import {Cricket} from './Models/Cricket';
+
+export const crickets: Cricket [] = [
+  {playerName: "Virat Kohli", playerJerseyNumber:18, playerPosition: "Batsman", playerAge: 35, isplayerawards: true, bgClass: "pink-bg"},
+{playerName: "Rohit Sharma", playerPosition: "Batsman", playerJerseyNumber: 45,playerAge: 36, isplayerawards: true, bgClass: "teal-bg"},
+{playerName: "Jasprit Bumrah", playerJerseyNumber: 93, playerPosition: "Bowler", playerAge: 30, isplayerawards: true, bgClass: "pink-bg"},
+{playerName: "Ravindra Jadeja", playerJerseyNumber: 8, playerPosition: "All-Rounder", playerAge: 35, isplayerawards: true, bgClass: "teal-bg"},
+{playerName: "KL Rahul", playerJerseyNumber: 1, playerPosition: "Wicketkeeper-Batsman", playerAge: 32, isplayerawards: true, bgClass: "pink-bg"},
+{playerName: "Shubman Gill", playerJerseyNumber: 77, playerPosition: "Batsman", playerAge: 24, isplayerawards: true, bgClass: "teal-bg"}
+
+];
