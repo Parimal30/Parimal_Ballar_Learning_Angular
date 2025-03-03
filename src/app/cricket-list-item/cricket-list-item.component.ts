071751f39eba22e3f04290d7f8173b2ff8c64d16
“Raj <w0836062@myscc.ca>
2025-03-03 16:12:00 -0500

    completed step 4

---
+++ src/app/cricket-list-item/cricket-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cricket-list-item',
+  imports: [],
+  templateUrl: './cricket-list-item.component.html',
+  styleUrl: './cricket-list-item.component.css'
+})
+export class CricketListItemComponent {
+
+}
---
+++ src/app/cricket-list/cricket-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cricket-list',
+  imports: [],
+  templateUrl: './cricket-list.component.html',
+  styleUrl: './cricket-list.component.css'
+})
+export class CricketListComponent {
+
+}
