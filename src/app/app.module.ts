82ef56b441473edc28f7676093f0fe80b215a1ea
“Parimal <W0851103@myscc.ca>
2025-03-12 22:10:07 -0400

    Started assignment 6 and completed step1

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  bootstrap: [],
+  declarations: [],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppComponent,
+  ],
+  providers: [],
+})
+export class AppModule {}
